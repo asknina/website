d5251e31d29e769915d92dc8bf847da8da0a9098
Srishti Nath <srishti.nath@gmail.com>
2024-03-06 01:10:46 -0500

    adding tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,36 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primaryPink: "#F267ED",
+        primaryPurple: "#423EEE",
+        pink: {
+          100: "#FF95FB",
+          200: "#FFAEFC"
+        },
+        gray: {
+          100: "#ECECF1",
+          200: "#C5C5D1",
+          300: "#9A9B9F",
+          400: "#444654"
+        },
+        orange: {
+          100: "#FEF8C4", // light tan
+          200: "#E6B52C", // subdued orange
+          300: "#F9A277", // light reddish orange
+          400: "#D56450" // reddish orange
+        },
+        midGreen: "#04BC00",
+        cerulean: "#4CA7DC",
+        bgPurple: "#6350D5",
+        yellowGreen: "#D9DC4C",
+        paleGreen: "#B9EE3E",
+        brightLightBlue: "#3EEBEE",
+        brightYellow: "#FFE400",
+        peach: "#F97777"
+
+      },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
