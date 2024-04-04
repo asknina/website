51e3daa7bd38a8fa6fdecef420be97642d1692b7
Srishti Nath <srishti.nath@gmail.com>
2024-04-04 12:01:14 -0400

    Update page.tsx

--- src/app/fund/page.tsx
+++ src/app/fund/page.tsx
@@ -12,13 +12,13 @@ export const metadata: Metadata = {
 const Fund = () => {
   return (
     <Container containerStyles="p-0 min-h-[90vH] shadow-xl z-30 flex flex-col">
-      <Container containerStyles="items-center justify-center flex flex-1 p-12">
-        <div className="text-center flex flex-col space-y-8 max-w-screen-lg">
-          <div className="text-6xl font-display">
+      <Container containerStyles="items-center justify-center flex flex-1 p-4 md:p-12 w-full">
+        <div className="text-center flex flex-col space-y-8 w-full max-w-screen-lg p-6">
+          <div className="text-5xl md:text-6xl font-display w-full">
             Introducing Ask Nina AI&apos;s Fund for girls and young women of
             color
           </div>
-          <div className="text-2xl">
+          <div className="text-xl md:text-2xl w-full m-4">
             Only 2% of funding in the U.S. is targeted to women and girls of
             color
           </div>
