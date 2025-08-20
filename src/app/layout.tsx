d208ba511b87c29816d2d1dd8a1d21db1349cc32
Srishti Nath <srishti.nath@gmail.com>
2025-08-19 22:09:43 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import GlitterCursor from "@/components/body/GlitterCursor";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 const nunito = Nunito({ subsets: ["latin"] });
-
+export const runtime = "edge";
 export const metadata: Metadata = {
   title: "Ask Nina AI | AI agent for teen girls exploring STEM careers",
   description:
@@ -26,9 +26,8 @@ export default function RootLayout({
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || ""} />
       <GoogleTagManager gtmId={process.env.NEXT_GTM_ID || ""} />
       <body className={nunito.className}>
-      <GlitterCursor />
-      <div className="bg-white min-h-screen w-full flex flex-col text-black">
-
+        <GlitterCursor />
+        <div className="bg-white min-h-screen w-full flex flex-col text-black">
           <Header />
           <Banner />
           <div className="flex-1">{children}</div>
